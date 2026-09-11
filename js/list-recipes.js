54d00e2c3027e737c33e62ea644d4366c66e81c3
echavez <22978299+veducha@users.noreply.github.com>
2026-09-11 22:32:16 +0100

    Display markdown title on index page and sort recipes alphabetically by title

--- js/list-recipes.js
+++ js/list-recipes.js
@@ -1,38 +1,48 @@
 
-// once document is loaded, load list of markdown files
-// and generate table of contents, plus a quick-nav list
-// at the top
+// once document is loaded, load list of recipes
+// and generate table of contents, plus a quick-nav list at the top
 $(document).ready(function() {
   let listOfRecipes = '';
   let listOfLetters = '';
   let prevLetter = '';
-  
-  // create list of recipes
-  for (let i in files) {
-    let url = files[i];
-    
-    // skip files that start with underscore
-    // (such as the _template.md file)
-    if (url[0] === '_') {
+
+  // Use recipe objects if available, otherwise construct from files
+  let recipeList = [];
+  if (typeof recipes !== 'undefined') {
+    recipeList = recipes;
+  } else if (typeof files !== 'undefined') {
+    recipeList = files.map(function(f) {
+      let slug = f.replace('.md', '');
+      return {
+        slug: slug,
+        title: slug.split('-').join(' '),
+        filename: f
+      };
+    });
+  }
+
+  for (let i = 0; i < recipeList.length; i++) {
+    let item = recipeList[i];
+
+    // skip files that start with underscore (such as template files)
+    if (item.filename && item.filename.charAt(0) === '_') {
       continue;
     }
 
-    // create anchor and name from url
-    let anchor = url.replace('.md', '');
-    let name = anchor.split('-').join(' ');
+    let title = item.title;
+    let anchor = item.slug;
 
     // if the first letter of the recipe hasn't been
-    // seen yet, add to list of letters and put an achor in
-    let firstLetter = name.charAt(0).toUpperCase();
+    // seen yet, add to list of letters and put an anchor in
+    let firstLetter = title.charAt(0).toUpperCase();
     if (firstLetter !== prevLetter) {
       listOfRecipes += '<li id="' + firstLetter + '">';
       listOfLetters += '<a href="#' + firstLetter + '">' + firstLetter + ' </a>';
-    }
-    else {
+    } else {
       listOfRecipes += '<li>';
     }
 
-    listOfRecipes += '<a href="recipe.html#' + anchor + '">' + name + '</a></li>';
+    listOfRecipes += '<a href="recipe.html#' + anchor + '">' + title + '</a></li>';
     prevLetter = firstLetter;
   }
 
@@ -42,4 +52,3 @@ $(document).ready(function() {
   // ...and the list of first-letters for quick nav
   $('#navigation').html(listOfLetters);
 });
-
